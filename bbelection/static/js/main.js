7242fd3b89732d1175dc71043fe91b44d04ff838
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-01 17:03:51 -0400

    completed request of votes by candidates

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,10 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    const getRoot = document.querySelector('.get_root');
-
-    getRoot.addEventListener('click', () => {
-        axios.get('https://bb-election-api.herokuapp.com/').then(function(response) {
-            console.log("works")
-        });
+    const candidates = document.querySelector('.candidates');
+    const candidatesRequest = axios.get('https://bb-election-api.herokuapp.com/');
+    candidatesRequest.then(function(response){
+        for (let i = 0; i < response.data['candidates'].length; i++){
+            console.log(response.data['candidates'][i]);
+            let candidate = response.data['candidates'][i];
+            let candidateEntry = document.createElement('li');
+            candidateEntry.innerHTML = `${candidate['name']} - ${candidate['votes']} Votes`;
+            candidates.appendChild(candidateEntry)
+        };
     })
-
 });
