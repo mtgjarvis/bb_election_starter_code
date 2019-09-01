16606bb70e5684da6818f0984e15d51bb0b4f6b9
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-01 17:52:41 -0400

    added vote button and eventListener

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const candidates = document.querySelector('.candidates');
-    const candidatesRequest = axios.get('https://bb-election-api.herokuapp.com/');
+    let candidatesRequest = axios.get('https://bb-election-api.herokuapp.com/');
     candidatesRequest.then(function(response){
         for (let i = 0; i < response.data['candidates'].length; i++){
             console.log(response.data['candidates'][i]);
@@ -9,6 +9,28 @@ document.addEventListener("DOMContentLoaded", function() {
             let candidateEntry = document.createElement('li');
             candidateEntry.innerHTML = `${candidate['name']} - ${candidate['votes']} Votes`;
             candidates.appendChild(candidateEntry)
+
+            const voteForm = document.createElement('form')
+            voteForm.method = 'POST'
+            voteForm.action = `https://bb-election-api.herokuapp.com/vote?id=${candidatesRequest.id}`
+            voteForm.setAttribute('name', 'id')
+        
+            const voteButton = document.createElement('button')
+            voteButton.type = 'submit'
+            voteButton.value = 'Vote'
+            voteButton.textContent = "VOTE"
+        
+            voteForm.appendChild(voteButton)
+            candidates.appendChild(voteForm)
+        
+            voteButton.addEventListener('click', (e) => {
+                e.preventDefault()
+                let vote = axios.post(`https://bb-election-api.herokuapp.com/vote?id=${candidatesRequest.id}`)
+                .then(function(response) {
+                    console.log('___Voted')
+                });
+            });
         };
     })
+ 
 });
