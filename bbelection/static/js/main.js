4dbefae4e4a4103a9e8bbb287739c3798f516312
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-01 16:18:08 -0400

    got first axios request completed

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const getRoot = document.querySelector('.get_root');
+
+    getRoot.addEventListener('click', () => {
+        axios.get('https://bb-election-api.herokuapp.com/').then(function(response) {
+            console.log("works")
+        });
+    })
 
 });
